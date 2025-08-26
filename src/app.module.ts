cedf206b77a7324ab3436f0f341f338ce63bba70
Khushal Khunt <59960215+FTW-Khushal@users.noreply.github.com>
2025-08-26 16:56:43 -0700

    Normalize line endings and run Prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardsModule } from './boards/boards.module';
+import { ColumnsModule } from './columns/columns.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [BoardsModule, ColumnsModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
