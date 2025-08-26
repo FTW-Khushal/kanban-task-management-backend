3f5eeb876226b36e26147045cbd6af01d3895d70
Khushal Khunt <59960215+FTW-Khushal@users.noreply.github.com>
2025-08-26 16:08:15 -0700

    feat: add CRUD operations for boards and integrate Swagger for API documentation

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,6 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { boards } from 'generated/prisma';
+import { boards, Prisma } from '@prisma/client';
 
 @Controller('boards')
 export class BoardsController {
@@ -9,5 +18,24 @@ export class BoardsController {
   @Get()
   async getAllBoards(): Promise<boards[]> {
     return this.boardsService.getAllBoards();
+  }
+
+  @Post()
+  async createBoard(@Body() board: Prisma.boardsCreateInput): Promise<boards> {
+    return this.boardsService.createBoard(board);
+  }
+
+  @Patch(':id')
+  async updateBoard(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: Prisma.boardsUpdateInput,
+  ): Promise<boards> {
+    console.log('Controller: Updating board with ID:', id);
+    return this.boardsService.updateBoard(id, data);
+  }
+
+  @Delete(':id')
+  async deleteBoard(@Param('id') id: string): Promise<boards> {
+    return this.boardsService.deleteBoard(Number(id));
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { boards } from 'generated/prisma';
+import { Prisma, boards } from '@prisma/client';
 
 @Injectable()
 export class BoardsService {
@@ -8,5 +8,26 @@ export class BoardsService {
 
   async getAllBoards(): Promise<boards[]> {
     return this.prisma.boards.findMany();
+  }
+
+  async createBoard(board: Prisma.boardsCreateInput): Promise<boards> {
+    return this.prisma.boards.create({ data: board });
+  }
+
+  async updateBoard(
+    id: number,
+    data: Prisma.boardsUpdateInput,
+  ): Promise<boards> {
+    console.log('Updating board with I:', id);
+    return this.prisma.boards.update({
+      where: { id },
+      data: data,
+    });
+  }
+
+  async deleteBoard(id: number): Promise<boards> {
+    return this.prisma.boards.delete({
+      where: { id },
+    });
   }
 }
